49421302c456cfe9a2ea3004990d6add45c60859
Vikash Kumar <vikash9932@gmail.com>
2022-08-22 17:19:30 +0530

    feat: game completed

---
+++ src/components/SingleCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './SingleCard.css';
+
+const SingleCard = ({ card, handleChoice, flipped, disabled }) => {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+  return (
+    <div className='card'>
+      <div className={flipped ? 'flipped' : ''}>
+        <img src={card.src} className='front' alt='card-front' />
+        <img
+          src='/img/cover.png'
+          className='back'
+          alt='card-back'
+          onClick={handleClick}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SingleCard;
